75ce06d4d2ff395828856ec25e954923d892d0bb
yasirdev111 <153387850+yasirdev111@users.noreply.github.com>
2025-03-12 20:12:16 -0700

    Create setupeve.sh

---
+++ setupeve.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# installing kind
+[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.27.0/kind-linux-amd64
+sudo chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+# installing kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
