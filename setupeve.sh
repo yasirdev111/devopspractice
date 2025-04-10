8828d65ed6952f0b833b707486d24cfd19ec0923
yasirdev111 <153387850+yasirdev111@users.noreply.github.com>
2025-04-10 08:03:18 -0700

    Update setupeve.sh

--- setupeve.sh
+++ setupeve.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 # installing kind
-sudo apt update
-sudo apt upgrade -y
-sudo apt update
 sudo apt install docker.io -y
 sudo usermod -aG docker $USER
 newgrp docker
-[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.27.0/kind-linux-amd64
+sudo curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.27.0/kind-linux-amd64
 sudo chmod +x ./kind
 sudo mv ./kind /usr/local/bin/kind
 # installing kubectl
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
